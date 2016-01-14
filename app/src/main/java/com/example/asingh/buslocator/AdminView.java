41e05d71909d210d72c1083922da5fcb4bf807cb
Avi Singh <iavi4@yahoo.com>
2016-01-14 11:59:50 -0500

    Finished AdminView.xml

--- app/src/main/java/com/example/asingh/buslocator/AdminView.java
+++ app/src/main/java/com/example/asingh/buslocator/AdminView.java
@@ -1,7 +1,15 @@
 package com.example.asingh.buslocator;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by 2016asingh on 1/5/2016.
  */
-public class AdminView {
+public class AdminView extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_view);
+    }
 }
